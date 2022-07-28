e612896f2aa4eab8fdc4055b000515dc53f8f248
Murat Kurtulus <murat-kurtulus@outlook.com>
2022-07-29 01:10:41 +0300

    testFail-expectRevert

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract Counter {
+    uint256 public num;
+
+    function increment() public {
+        num++;
+    }
+
+    function decrement() public {
+        num--;
+    }
+
+    function getNum() public view returns (uint256) {
+        return num;
+    }
+}
--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,41 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Contract.sol";
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Counter counter;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        counter = new Counter();
+    }
+
+    function testNumZero() public {
+        assertEq(counter.num(), 0);
+    }
+
+    function testIncrementByOne() public {
+        emit log_named_uint("num", counter.num());
+        counter.increment();
+        assertEq(counter.getNum(), 1);
+    }
+
+    function testDecrementByOne() public {
+        counter.increment();
+        counter.increment();
+        emit log_named_uint("num", 2);
+        counter.decrement();
+        assertEq(counter.getNum(), 1);
+    }
+
+    function testFailDecrementByOne() public {
+        counter.decrement();
+        assertEq(counter.getNum(), 0);
+    }
+
+    function testCannotDecrementByOne() public {
+        vm.expectRevert(stdError.arithmeticError);
+        counter.decrement();
+        assertEq(counter.getNum(), 0);
     }
 }
